f5f2dfeb2df13b2cfc2901364141c0613691e3c5
VivianBrandenburg <code@vivian-brandenburg.de>
2022-01-24 12:56:57 +0100

    fix

--- split2transcripts.py
+++ split2transcripts.py
@@ -5,12 +5,12 @@ from tools.box import write_scores, write_transcriptome
 import argparse
 
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(usage='%(prog)s genome_file gff_file outfile [-h] [-f] [-n]')
 parser.add_argument("genome_file", type=str, help="infile of genome or scores")
 parser.add_argument("gff_file", type=str, help="gff infile")
 parser.add_argument("outfile", type=str, help="outfile")
-parser.add_argument('-f', '--feature' ,type=str, help="gff feature to use. Default: CDS", default='CDS', metavar='' ,)
-parser.add_argument("-n", '--name', type=str, help="identifier to use from gff. Default: locus_tag", default='locus_tag', metavar='')
+parser.add_argument('-f', '--feature' ,type=str, help="gff feature to use. Default is 'CDS'", default='CDS', metavar='' ,)
+parser.add_argument("-n", '--name', type=str, help="identifier to use from gff. Default is 'locus_tag'", default='locus_tag', metavar='')
 args = parser.parse_args()
 
 
